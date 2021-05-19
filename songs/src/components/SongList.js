6baac4ccdf515245d2560a3fba1d39f0b72c45b1
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-05-19 18:49:06 +0500

    added components

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,9 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
-    render() {
-        return <div>SongList</div>;
-    }
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button 
+            className="ui button prima"
+            onClick={() => this.props.selectSong(song)}>
+                Select
+                </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+      console.log(this.props)
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+    console.log(state)
+  return { songs: state.songs };
+};
+
+//Connect is  function that invokes another function
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -18,6 +18,6 @@ const selectedSongReducer = (selectedSong = null, action) => {
 };
 
 export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
 });
